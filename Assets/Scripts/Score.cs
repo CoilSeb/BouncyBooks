1c68c0d9ad0a9712442909530a39412fc89d8d5f
Sebastian Shirk <sebasshirk@icloud.com>
2023-09-14 13:28:09 -0500

    oush

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Score : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform Player;
+    public Text scoreText;
 
     // Update is called once per frame
     void Update()
     {
-        
+        // how to convert string to float: float.Parse(scoreText.text)
+        // if the players velocity is > 0 scoreText.text = Player.position.y.ToString("0");
+        if (float.Parse(scoreText.text) < Player.position.y)
+        {
+            scoreText.text = Player.position.y.ToString("0");
+        }
+
     }
 }
